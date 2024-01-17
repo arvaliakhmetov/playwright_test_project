9de2cb1246beca3a9016c96fa4b4e5f1196cef3b
Avaliakhmetov <AValiakhmetov@blogic.ru>
2024-01-17 14:33:17 +0500

    -

--- src/main/kotlin/core/TestActionExecutor.kt
+++ src/main/kotlin/core/TestActionExecutor.kt
@@ -18,12 +18,12 @@ class TestActionExecutor(
 
     fun executeAction(vararg pageActions: PageRoute) {
         pageActions.forEach { pageAction ->
-            logger.info("${pageAction.javaClass.simpleName} BEFORE ACTION: $state")
+
             LoadingControl.waitForPageLoading(page)
+
             pageAction.getAction(page,state)
+
             LoadingControl.waitForPageLoading(page)
-            //state = pageAction.runAction(state = state, page = page)
-            logger.info("${pageAction.javaClass.simpleName} AFTER ACTION: $state")
         }
     }
 
