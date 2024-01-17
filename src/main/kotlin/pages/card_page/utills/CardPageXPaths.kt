5ede423a6fd21d4d0879226ef684e6901459aacf
Avaliakhmetov <AValiakhmetov@blogic.ru>
2024-01-17 15:01:26 +0500

    -

--- src/main/kotlin/pages/card_page/utills/CardPageXPaths.kt
+++ src/main/kotlin/pages/card_page/utills/CardPageXPaths.kt
@@ -3,11 +3,11 @@ package org.example.pages.card_page.utills
 
 
 object CardPageXPaths {
-    val SPINNER = "//img[@src='/UZDO/webjars/dojox/widget/Standby/images/loading.gif']"
-    val TOOLBAR_BUTTON = "//div[@role='tabpanel' and contains(@class,'dijitVisible') and contains(@aria-labelledby,'Card') or contains(@id,'DocumentResolver')]//div[@role='toolbar' and not(contains(@style,'display: none;'))]//span[@aria-disabled='false']//span[@class[contains(.,'Button')] and text()='%1%']"
-    val DROPDOWN_ITEM = "//div[contains(@class,'dijitReset dijitMenuItem')]//span[.='%1%']"
-    val FIELD = "(//div[@title='%1%'])[last()]//div[@class[contains(.,'Input')]]/input[1]"
-    val LOAD_FILE = "//span[contains(text(),'Добавить')]/preceding::input[@type='file' and not(contains(@style,' display: none;'))]"
-    val TAB = "(//div[@role='tabpanel' and contains(@class,'dijitVisible') and contains(@aria-labelledby,'Card') or contains(@id,'DocumentResolver')]//span[@class[contains(.,'tabLabel')] and text()='%1%'])[last()]"
+    const val SPINNER = "//img[@src='/UZDO/webjars/dojox/widget/Standby/images/loading.gif']"
+    const val TOOLBAR_BUTTON = "//div[@role='tabpanel' and contains(@class,'dijitVisible') and contains(@aria-labelledby,'Card') or contains(@id,'DocumentResolver')]//div[@role='toolbar' and not(contains(@style,'display: none;'))]//span[@aria-disabled='false']//span[@class[contains(.,'Button')] and text()='%1%']"
+    const val DROPDOWN_ITEM = "//div[contains(@class,'dijitReset dijitMenuItem')]//span[.='%1%']"
+    const val FIELD = "(//div[@title='%1%'])[last()]//div[@class[contains(.,'Input')]]/input[1]"
+    const val LOAD_FILE = "//span[contains(text(),'Добавить')]/preceding::input[@type='file' and not(contains(@style,' display: none;'))]"
+    const val TAB = "(//div[@role='tabpanel' and contains(@class,'dijitVisible') and contains(@aria-labelledby,'Card') or contains(@id,'DocumentResolver')]//span[@class[contains(.,'tabLabel')] and text()='%1%'])[last()]"
 }
 
--- src/main/kotlin/pages/login_page/utills/LoginPageXPaths.kt
+++ src/main/kotlin/pages/login_page/utills/LoginPageXPaths.kt
@@ -3,7 +3,7 @@ package org.example.pages.login_page.configs
 
 
 object LoginPageXPaths {
-    val INPUT = "//label[contains(text(),'%1%')]/following::input[1]"
-    val BUTTON= "//span[text()='%1%']/ancestor::span[@role='button']"
+    const val INPUT = "//label[contains(text(),'%1%')]/following::input[1]"
+    const val BUTTON= "//span[text()='%1%']/ancestor::span[@role='button']"
 }
 
